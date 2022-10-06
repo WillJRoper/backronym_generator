b4caa36ae48d159faa06c05fc5cc5ee372fd15f2
wjr21 <wjr21@sussex.ac.uk>
2022-10-06 13:33:54 +0100

    Working base algorithm with no scoring, needs speeding up too

--- backropy/main.py
+++ backropy/main.py
@@ -9,19 +9,34 @@ import nltk.corpus
 import sys
 import nltk
 import random
+import numpy as np
+import itertools
 
 
-def backronym_loop(acronym, words):
-    output = ''
-    random.shuffle(words)
-    for letter in acronym:
-        for j, word in enumerate(words):
-            if letter.lower() in word.lower():
-                output += word.lower().replace(letter, letter.upper(), 1)+' '
-                words.pop(j)
-                break
+def backronym_loop(acronym, words, ini_synsets):
 
-    return output.strip()
+    # Set up lists to hold each letters words
+    words_for_letters = []
+    while len(words_for_letters) < len(acronym):
+        words_for_letters.append([])
+
+    # Loop over letters in the desired acronym
+    for ind, l in enumerate(acronym):
+
+        # Loop over words in the bag of words and include them if the letter
+        # is in them
+        for w in words:
+            if l in w.lower():
+                words_for_letters[ind].append(w)
+
+    # Create a list to store the list of backacronyms
+    backronyms = list(itertools.product(*words_for_letters))
+
+    # Remove duplicates
+    backronyms = sorted(backronyms)
+    backronyms = list(k for k, _ in itertools.groupby(backronyms))
+
+    return backronyms
 
 
 def main():
@@ -38,13 +53,21 @@ def main():
     nltk.download('wordnet')
 
     # Get acronym and bag of words
-    n_results = int(sys.argv[1])
-    acronym = sys.argv[2].lower()
-    bag_of_words = sys.argv[3:]
+    n_results = 10
+    acronym = sys.argv[1].lower()
+    ini_bag_of_words = sys.argv[2:]
+    bag_of_words = []
+    bag_of_words.extend(ini_bag_of_words)
 
     # Include some stop words that could be useful
     bag_of_words.extend(["the", "and", "is", "of", "in", "for",
                          "from", "or", "a", "an", "by"])
+
+    # Create a list of synsets for the initial bag of words for
+    # similarity computing
+    ini_synsets = []
+    for word in ini_bag_of_words:
+        ini_synsets.extend(wordnet.synsets(word))
 
     # Set up list for the synonyms
     synonyms = []
@@ -54,17 +77,35 @@ def main():
 
         # Loop over synonyms for this word
         for syn in wordnet.synsets(word):
-            for i in syn.lemmas():
-                synonyms.append(i.name())
+
+            # Store maximum similarity to initial words
+            max_sim = 0
+            for ini_syn in ini_synsets:
+                d = wordnet.wup_similarity(ini_syn, syn)
+
+                if d > max_sim:
+                    max_sim = d
+
+            # Only assign these synonyms if sufficiently similar
+            if max_sim > 0.75:
+
+                for i in syn.lemmas():
+
+                    # Ensure this word contains letters in the acronym
+                    for l in acronym:
+                        if l in i.name():
+                            synonyms.append(i.name())
+                            break
 
     # Combine the synonyms into the bag of words
     bag_of_words.extend(set(synonyms))
 
-    # Loop until desired number of backronyms have been generated
+    # MAIGC
+    backronyms = backronym_loop(acronym, bag_of_words, ini_synsets)
+
+    # Loop over backronyms print out n top results
     i = 0
     while i < n_results:
-        # MAIGC
-        backronym = backronym_loop(acronym, bag_of_words)
 
-        print("Backronym: %s" % backronym)
+        print("Backronym:", backronyms[i])
         i += 1
